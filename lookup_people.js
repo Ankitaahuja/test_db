ffda89c60e0bda2f3d6cbc604b73bea4575fe4c5
ankitaahuja111 <ankitaahuja111@gmail.com>
2018-09-18 17:22:25 +0000

    made changes in lookup_people.js file to get the ids

--- lookup_people.js
+++ lookup_people.js
@@ -24,9 +24,9 @@ client.connect((err) => {
       return console.error("error running query", err);
     }
     console.log(`found '${result.rows.length}' person(s) by the name '${name}'`); 
-
+let id = 1
     result.rows.forEach(x => {
-        const final = `- ${x.id}, ${x.first_name} ${x.last_name}, born '${x.birthdate.toLocaleString().slice(0,-9)}'`
+        const final = `- ${id++}, ${x.first_name} ${x.last_name}, born '${x.birthdate.toLocaleString().slice(0,-9)}'`
         console.log(final);
     })
     client.end();
